53f6cec849b02684ad469c0ef5ab6e8fcb2c438d
Greg Trahair <greg.trahair@gmail.com>
2012-02-21 20:54:20 +0100

    Initial commit

---
+++ pyrthings.py
@@ -0,0 +1,28 @@
+'''A set of random things.'''
+import random
+
+def random_mac():
+    '''Create a random MAC address'''
+    mac = [ random.randint(0x00, 0xff),
+            random.randint(0x00, 0xff),
+            random.randint(0x00, 0xff),
+            random.randint(0x00, 0xff),
+            random.randint(0x00, 0xff),
+            random.randint(0x00, 0xff) ]
+    return ':'.join(['%02x' % m for m in mac])
+
+def random_asset_tag():
+    '''Create a random, zero padded, 6 digit number'''
+    return ('%06d' % random.randint(1, 999999))
+
+def random_ports(port_prefix='eth', amount=5):
+    '''create a random amount of ethernet port names between 2 and option,
+    amount.
+    
+    '''
+    ports_to_create = []
+    no_of_ports = random.randint(2, amount)
+    for port in xrange(1, no_of_ports):
+        if port != None:
+            ports_to_create.append(port_prefix + str(port))
+    return ports_to_create
