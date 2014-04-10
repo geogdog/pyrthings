8f0eef806636d8282c7e0b7b4c6a9dd400254bb2
Greg Trahair <greg.trahair@gmail.com>
2014-04-10 14:24:37 +0200

    Made random_mac faster by making the kernel do the work and added random IP generators

--- pyrthings.py
+++ pyrthings.py
@@ -1,34 +1,46 @@
 '''A set of random things.'''
 from __future__ import print_function
-import random
+from random import randint
+from struct import unpack_from
+from os import urandom
+import netaddr
+
 
 def random_mac():
     '''Create a random MAC address.'''
-    mac = [ random.randint(0x00, 0xff),
-            random.randint(0x00, 0xff),
-            random.randint(0x00, 0xff),
-            random.randint(0x00, 0xff),
-            random.randint(0x00, 0xff),
-            random.randint(0x00, 0xff) ]
-    return ':'.join(['%02x' % m for m in mac])
+    return '{0:02x}:{1:02x}:{2:02x}:{3:02x}:{4:02x}:{5:02x}'.format(
+        *unpack_from('BBBBBB',urandom(20)))
 
 def random_asset_tag():
     '''Create a random, zero padded, 6 digit number.'''
-    return ('%06d' % random.randint(1, 999999))
+    return ('%06d' % randint(100000, 999999))
 
-def random_ports(port_prefix='eth', amount=5):
+def random_ports(port_prefix='eth', amount=4):
     '''create a random amount of ethernet port names between 2 and option,
     amount.
-    
+
     '''
     ports_to_create = []
-    no_of_ports = random.randint(2, amount)
-    for port in xrange(1, no_of_ports):
+    no_of_ports = randint(1, amount)
+    for port in xrange(0, no_of_ports):
         if port != None:
             ports_to_create.append(port_prefix + str(port))
     return ports_to_create
 
+def random_ip_in_network(network):
+    network = netaddr.IPNetwork(network)
+    return network[randint(0, len(network))]
+
+def random_ip_in_range(start, end):
+    range = netaddr.IPRange(start, end)
+    return range[randint(0, len(range))]
+
+
 if __name__ == '__main__':
     print('Random MAC: %s' % random_mac())
     print('Random Asset Tag: %s' % random_asset_tag())
-    print('Random Ports: %s' % ' ,'.join(random_ports()))
+    print('Random Ports: %s' % ', '.join(random_ports()))
+    print('Random IP in 10.0.0.0/8 network: {0}'.format(
+        random_ip_in_network(network='10.0.0.0/8')))
+    print('Random IP between 192.168.1.3 and 192.168.3.87: {0}'.format(
+        random_ip_in_range('192.168.1.3', '192.168.3.87')))
--- test_all.py
+++ test_all.py
@@ -3,10 +3,22 @@ from pyrthings import random_mac, random_asset_tag, random_ports
 
 class TestSequenceFunctions(unittest.TestCase):
     def test_random_mac(self):
-        self.assertEqual(len(random_mac()), 17)
+        mac = random_mac()
+        self.assertEqual(len(mac), 17)
 
     def test_random_asset_tag(self):
-        self.assertEqual(len(random_asset_tag()), 6)
+        asset_tag = random_asset_tag()
+        self.assertEqual(len(asset_tag), 6)
 
     def test_random_ports(self):
-        self.assertTrue(isinstance(random_ports(), list))
+        ports = random_ports()
+        self.assertTrue(isinstance(ports, list))
+
+    def test_random_ip_in_network(self):
+        pass
+
+    def test_random_ip_in_range(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
